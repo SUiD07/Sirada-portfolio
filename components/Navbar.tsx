e9adf8c56680beee567be86c79403485cf6c1d3d
Sirada U <nan.uthaiwat@gmail.com>
2024-12-23 14:01:26 +0700

    initial commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+export default function Navbar(){
+    return(
+        <>
+        <div className="bg-white shadow-md h-14 flex items-center">
+            <div className="text-base font-bold mx-5">My portfolio</div>
+            <div className="">
+                
+            </div>
+        </div>
+        </>
+    )
+}
