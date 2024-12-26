ebb1489f03eb48eb1784cb866ef1d18162096283
Sirada U <nan.uthaiwat@gmail.com>
2024-12-26 22:35:23 +0700

    feat: add animation

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -1,9 +1,12 @@
 export default function Homepage() {
   return (
     <>
-      <div className="text-9xl my-5 max-md:text-7xl mt-20">
+      <div className="inline-block hover:text-9xl hover:bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-500 hover:text-transparent hover:bg-clip-text duration-1000 text-8xl my-5 max-md:text-7xl mt-20">
         Sirada <br />
         Uthaiwat
+      </div>
+      <div className="rounded-md border-black p-4 shadow-md mx-10 max-md:my-5 inline-flex items-center">
+        Mouse over this element and <br/>see what happen!
       </div>
       <div className="mx-3">
         <p>Found this website's source code in GitHub</p>
--- components/Work.tsx
+++ components/Work.tsx
@@ -1,6 +1,6 @@
 import HeadSec from "./HeadSec";
 import { workList } from "@/constants/workList";
-import  githubprofile  from "/public/githubprofile.png";
+import githubprofile from "/public/githubprofile.png";
 
 import Image from "next/image";
 
@@ -8,12 +8,18 @@ export default function Work() {
   return (
     <>
       <HeadSec header="Work" />
-      <div className="py-3">See my GitHub profile and my contributions here</div>
-      <a
+      <div className="inline-block">
+        <div className="py-3">
+          See my GitHub profile and my contributions here
+        </div>
+        <a
           href="https://github.com/SUiD07"
           className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-md p-2 font-semibold text-white"
-        >Click here</a>
-      <Image src={githubprofile} alt="profile" className="my-3" />
+        >
+          Click here
+        </a>
+      </div>
+      <Image src={githubprofile} alt="profile" className="hover:w-1/2 max-md:hover:w-5/6 duration-1000 my-1 inline-block w-96" />
       {/* <div className="text-xl font-bold">My work as a front-end developer</div>
       {workList.map((workItem, workIndex) => (
         <div className="shadow-md w-80 h-60 inline-block mx-4">
